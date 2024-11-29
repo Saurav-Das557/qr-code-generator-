560901f2dcad50cb33be7c3885501b0c4ec7b549
Saurav Das <sauravdas557@gmail.com>
2024-11-29 18:05:46 +0600

    fixed the alignment and the pop up

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-function generateQR(){
-    if(qrText.value.length>0){
-        qrImage.src=" https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
-        imgBox.classList.add("show-img")
-    }
-    else{
+function generateQR() {
+    const qrText = document.getElementById("qrText");
+    const imgBox = document.getElementById("imgBox");
+    const qrImage = document.getElementById("qrImage");
+
+    if (qrText.value.length > 0) {
+        qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(qrText.value)}`;
+        
+        qrImage.onload = () => {
+            imgBox.classList.add("show-img");
+        };
+
+        qrImage.onerror = () => {
+            imgBox.classList.remove("show-img");
+            qrText.classList.add('error');
+            setTimeout(() => {
+                qrText.classList.remove('error');
+            }, 1000);
+        };
+    } else {
         qrText.classList.add('error');
-        setTimeout(()=>{
+        setTimeout(() => {
             qrText.classList.remove('error');
         }, 1000);
     }
-
-    
 }
-
-
-let imgBox=document.getElementById("imgBox")
-let qrImage=document.getElementById("qrImage")
-let qrText=document.getElementById("qrText")
